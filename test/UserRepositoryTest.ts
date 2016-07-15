55983bda1cb814c7942acbd3efbaed20de9e3567
divramod <divramod@gmail.de>
2016-07-15 14:32:02 +0200

    added entity user

--- test/UserRepositoryTest.ts
+++ test/UserRepositoryTest.ts
@@ -8,23 +8,23 @@ let assert = chai.assert;
 
 describe("UserRepository", function() {
   it("Create a user", function(done) {
-  var repo = new UserRepository();
+    var repo = new UserRepository();
 
-  var user: IUser = {
-    _id: undefined,
-    name: "user",
-    description: "teste",
-    completed: false,
-    createdDate: undefined,
-    updatedAt: undefined
-  };
+    var user: IUser = {
+      _id: undefined,
+      name: "user",
+      description: "teste",
+      completed: false,
+      createdDate: undefined,
+      updatedAt: undefined
+    };
 
-  repo.create(user).then((createdUser) => {
-    assert.isNotNull(user._id);
-    assert.isNotNull(user.createdDate);
-    done();
-  }).catch((error) => {
-    done(error);
-  });
+    repo.create(user).then((createdUser) => {
+      assert.isNotNull(user._id);
+      assert.isNotNull(user.createdDate);
+      done();
+    }).catch((error) => {
+      done(error);
+    });
   });
 });
